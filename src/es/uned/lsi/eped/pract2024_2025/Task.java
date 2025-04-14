21d03feb737093941c46d292bfa568b494749be4
drodriguezgarces <diegorodriguezgarces@gmail.com>
2025-04-14 20:41:32 +0200

    commit inicial

---
+++ src/es/uned/lsi/eped/pract2024_2025/Task.java
@@ -0,0 +1,26 @@
+package es.uned.lsi.eped.pract2024_2025;
+
+public class Task implements TaskIF {
+
+	/* Declaración de atributos para almacenar la información de una tarea */
+
+	/* Marca la tarea como completada */
+	public void setCompleted() {}
+	
+	/* Devuelve el texto de la tarea */
+	public String getText() {
+		return null;}
+
+	/* Devuelve la fecha de la tarea */
+	public int getDate() {
+		return 0;}
+
+	/* Devuelve si la tarea ha sido completada o no */
+	public boolean getCompletion() {
+		return false;}
+
+	/* Compara la tarea actual con una tarea llamante */
+	public int compareTo(TaskIF T) {
+		return 0;}
+
+}
